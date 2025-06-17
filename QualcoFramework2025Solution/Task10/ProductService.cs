df8e66a83e96fa0a8e45cdfb3e30a275f235e7cc
Dimitris Iracleous <diracleous@athtech.gr>
2025-06-17 14:01:12 +0300

    minor

--- QualcoFramework2025Solution/Task10/ProductService.cs
+++ QualcoFramework2025Solution/Task10/ProductService.cs
@@ -2,27 +2,47 @@
 
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace QualcoLab2025.Task10
 {
 
-    public class Product
+    public class Order
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
+        public int CustomerId { get; set; }
+        public string Status { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+    public class AppDbContext
+    {
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
     }
 
-    public class ProductService
+    public class OrderServiceOld
     {
-        private static readonly HttpClient httpClient = new HttpClient();
+        public List<Order> GetCustomerOrders(int customerId)
+        {
+            var context = new AppDbContext();
+            var result = new List<Order>();
 
-        public async Task<Product> FetchProductAsync(int id)
-        { 
-            return null;  
+            foreach (var order in context.Orders)
+            {
+                if (order.CustomerId == customerId && order.Status != "Cancelled")
+                {
+                    result.Add(order);
+                }
+            }
+
+            result.Sort((a, b) => a.OrderDate.CompareTo(b.OrderDate));
+
+            return result;
         }
     }
-}
 
+    public class OrderService
+    {
+    }
+}
